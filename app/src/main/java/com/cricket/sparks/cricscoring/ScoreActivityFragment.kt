dcb777596fec3df4169734fd9bcf65ad3c31d9b2
Sailesh <sailesh1726@gmail.com>
2020-06-17 19:49:58 -0700

    updated project to support Kotlin

---
+++ app/src/main/java/com/cricket/sparks/cricscoring/ScoreActivityFragment.kt
@@ -0,0 +1,18 @@
+package com.cricket.sparks.cricscoring
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class ScoreActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_score, container, false)
+    }
+}
